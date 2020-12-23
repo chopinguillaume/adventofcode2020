024828b16abc2282866091195c04ebd2e7b56fbd
Guillaume <guillaumechopin35@gmail.com>
2020-12-23 16:36:59 +0100

    Day 15 optimization

--- src/main/java/fr/gchopin/advent2020/days/Day15.java
+++ src/main/java/fr/gchopin/advent2020/days/Day15.java
@@ -21,7 +21,8 @@ public class Day15 extends Day {
     }
 
     private void runUntilXTurns(int maxTurns) {
-        Map<Integer, Integer> seenNumbers = new HashMap<>(); // Key is the number, Value is the last turn
+        // An array is faster than a Map
+        int[] seenNumbers = new int[maxTurns];
 
         // Initialize with turn 0
         int lastNumber = input[0];
@@ -32,17 +33,16 @@ public class Day15 extends Day {
             if (turn < input.length) {
                 newNumber = input[turn];
             } else {
-                if (seenNumbers.containsKey(lastNumber)) {
-                    int lastTurnOfNumber = seenNumbers.get(lastNumber);
-                    int lastTurn = turn - 1;
-                    newNumber = lastTurn - lastTurnOfNumber;
+                int lastTurnOfNumber = seenNumbers[lastNumber];
+
+                if (lastTurnOfNumber != 0) {
+                    newNumber = turn - lastTurnOfNumber;
                 } else {
                     newNumber = 0;
                 }
             }
             // Here we know our new number, so we can store the previous one
-            // System.out.println(turn + " : " + lastNumber);
-            seenNumbers.put(lastNumber, turn - 1);
+            seenNumbers[lastNumber] = turn;
             // We prepare the next loop by making our new number the next 'previous one'
             lastNumber = newNumber;
         }
